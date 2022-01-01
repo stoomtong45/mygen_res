eb4f98f4af8500f3c1fa4a04018ca58187fdc455
stoomtong45 <46856608+stoomtong45@users.noreply.github.com>
2022-01-01 17:59:53 +0700

    Update art_gan.py

--- art_gan.py
+++ art_gan.py
@@ -26,7 +26,7 @@ IMAGE_SIZE = 128  # rows/cols
 IMAGE_CHANNELS = 3
 
 
-training_data = np.load('/spell/cubism_data.npy')
+training_data = np.load('/training_data/training_data.npy')
 
 
 def build_discriminator(image_shape):
