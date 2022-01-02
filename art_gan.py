7d498753b17e59addc3d4d81ae9ae7ae0eb23c89
stoomtong45 <46856608+stoomtong45@users.noreply.github.com>
2022-01-03 03:14:02 +0700

    Update art_gan.py

--- art_gan.py
+++ art_gan.py
@@ -28,7 +28,7 @@ IMAGE_SIZE = 128  # rows/cols
 IMAGE_CHANNELS = 3
 
 
-training_data = np.load('Generative02/training_data.npy')
+training_data = np.load('spell/training_data.npy')
 
 
 def build_discriminator(image_shape):
