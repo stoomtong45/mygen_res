72b8d10a6977d95211fb8bf23d96f6a731238956
stoomtong45 <46856608+stoomtong45@users.noreply.github.com>
2022-01-03 03:22:15 +0700

    Update art_gan.py

--- art_gan.py
+++ art_gan.py
@@ -2,7 +2,7 @@ from keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormaliza
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model, load_model
-from keras.optimizers import Adam
+from keras.optimizers import adam_v2
 import numpy as np
 from PIL import Image
 import os
@@ -131,7 +131,7 @@ def save_images(cnt, noise):
 
 image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
 
-optimizer = Adam(1.5e-4, 0.5)
+optimizer = adam_v2.Adam(1.5e-4, 0.5)
 
 discriminator = build_discriminator(image_shape)
 discriminator.compile(loss="binary_crossentropy",
